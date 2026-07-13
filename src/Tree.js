32a096936931e82f05c3bf015ab51857f447169a
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-07-13 16:09:21 -0400

    Fix Tree and TreeTest files

--- src/Tree.js
+++ src/Tree.js
@@ -1,4 +1,4 @@
-import Node from "./Node";
+import Node from "./Node.js";
 
 export default class Tree {
     #buildTree(arr, start, end){
--- src/TreeTest.js
+++ src/TreeTest.js
@@ -1,5 +1,5 @@
-import Tree from "./Tree";
-import {prettyPrint} from "./TreePrettyPrint"
+import Tree from "./Tree.js";
+import {prettyPrint} from "./TreePrettyPrint.js"
 
 let testTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 prettyPrint(testTree.root)
