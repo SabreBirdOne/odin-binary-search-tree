0130955f1beadc34d70fa4cf4b1f9b8ee8d7514e
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-07-13 15:57:26 -0400

    Add buildTree functions

--- src/Tree.js
+++ src/Tree.js
@@ -1,8 +1,24 @@
 import Node from "./Node";
 
 export default class Tree {
+    #buildTree(arr, start, end){
+        if (start > end) return null;
+        else {
+            const mid = Math.floor(start + (end - start) / 2);
+            let newRoot = new Node(arr[mid]);
+            newRoot.left = this.#buildTree(arr, start, mid - 1);
+            newRoot.right = this.#buildTree(arr, mid + 1, end);
+            return newRoot;            
+        }
+    }
+
+    #buildTreeStarter(arr){
+        const sortedArr = [...new Set(arr.toSorted((a, b) => a - b))];
+        return this.#buildTree(sortedArr, 0, sortedArr.length - 1);
+    }
+
     constructor(startArray = []){
-        this.root = null;
         this.size = 0;
+        this.root = this.#buildTreeStarter(startArray);
     }
 }
