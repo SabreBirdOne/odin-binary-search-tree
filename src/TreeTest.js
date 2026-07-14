8bf525c089f093a5c6b92cb830d077159aaeaf62
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-07-14 12:13:23 -0400

    Add test for empty callbacks to levelOrderForEach

--- src/TreeTest.js
+++ src/TreeTest.js
@@ -39,3 +39,9 @@ testTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
     console.log("After:");
     prettyPrint(testTree.root);
 })
+
+try {
+    testTree.levelOrderForEach();
+} catch (error){
+    console.log(error.message);
+}
